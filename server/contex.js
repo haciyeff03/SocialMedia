fbd0ae07c1ae91f2dd3982b543d5751e16ab3cfc
Your Name <haciyevqurban97@gmail,com>
2023-07-20 00:26:23 +0400

    models,controllers,routers is ready

---
+++ server/contex.js
@@ -0,0 +1,21 @@
+/* 
+0:00 Build a Fullstack MERN React Responsive Social Media Application from Scratch
+5:07 Backend Installation of Node, Visual Studio Code, and Backend Packages
+7:44 Backend Configurations and Middleware Setup
+16:06 MongoDB Registering, Installation and Setup
+21:31 Data Modeling and ERD Diagrams
+26:08 Authentication and Authorization in Node
+59:07 User Routes Setup
+1:13:33 Post Routes Setup
+1:36:20 Backend Data Add and Demo
+1:42:20 Frontend Installation and Setup
+1:48:34 React Redux File Folder Architecture and React Router
+1:56:10 Redux and Toolkit Installation and Setup
+2:10:56 Color, Theme, Dark Mode, and Styling Setup
+2:26:16 Navbar
+2:48:37 Register, Login Pages, and Form
+3:34:44 Home Page and Widgets
+4:04:17 Posts and Post Widgets
+5:19:41 Profile Page
+5:27:08 Full Stack MERN React Application Complete
+*/
