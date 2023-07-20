4d838e292a54c994c2d60d896edeb55a5b48c516
Your Name <haciyevqurban97@gmail,com>
2023-07-20 15:17:20 +0400

    login is created

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,4 +2,8 @@ import express  from "express";
 import {login} from "../controllers/auth.js";
 
 
-const router=express.Router
+const router=express.Router();
+router.post("/login",login)
+
+
+export default router;
