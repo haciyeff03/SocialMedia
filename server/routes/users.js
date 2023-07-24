a0e5ecdc3bbd35bd2c6bca6c436e51e04b3a0dec
Your Name <haciyevqurban97@gmail,com>
2023-07-24 19:30:19 +0400

    get user, friends update,remove,add,formatted

--- server/routes/users.js
+++ server/routes/users.js
@@ -2,8 +2,16 @@
  import {
     getUser,
     getUserFriends,
-    addRemoveFriends,
+    addRemoveFriend,
  } from  "../controllers/users.js";
  import { verifyToken } from "../middleware/auth.js";
  
  const router=express.Router();
+
+ /* read */ 
+ router.get("/:id",verifyToken,getUser);
+ router.get("/:id/friends",verifyToken,getUserFriends);
+
+
+ /* update */ 
+ router.patch("/:id/:friendId",verifyToken,addRemoveFriend);
