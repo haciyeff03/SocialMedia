3fd84621e9b6c4fd732cbf7c0f98b63a826928aa
Your Name <haciyevqurban97@gmail,com>
2023-08-08 23:51:13 +0400

    redux toolkit installation,configration

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home/Home";
 import Login from "./Pages/Login/Login";
-import Profile from "./Pages/Profile/Profil";
+import Profile from "./Pages/Profile/Profile";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.js';
 
--- client/src/Pages/Profile/Profil.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import "./profil.scss"
-const Profil = () => {
-  return (
-    <div>Profil</div>
-  )
-}
-
-export default Profil
