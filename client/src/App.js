55e92216a6bac3413f715f7a626e76ebffc7a93d
Your Name <haciyevqurban97@gmail,com>
2023-08-08 14:34:21 +0400

    app.js importance okey

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,21 @@
+import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home/Home";
+import Login from "./Pages/Login/Login";
+import Profile from "./Pages/Profile/Profil";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.js';
 
 function App() {
   return (
     <div className="App">
-    
-    
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Login/>}/>
+          <Route path="/home" element={<Home/>}/>
+          <Route path="/profile/:userId" element={<Profile/>}/>
+        </Routes>
+      </BrowserRouter>
+
     </div>
   );
 }
