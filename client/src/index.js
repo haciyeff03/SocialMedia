969de3bf4b06ad940020674456f15aa9cff6192f
Your Name <haciyevqurban97@gmail,com>
2023-08-11 11:56:47 +0400

    state management is completed

--- client/src/index.js
+++ client/src/index.js
@@ -2,11 +2,48 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import authReducer from "./state";
+import { configureStore } from '@reduxjs/toolkit';
+import { Provider } from 'react-redux';
+import{
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER
+} from "redux-persist";
+import { Storage } from 'redux-persist/lib/storage';
+import { PersistGate } from 'redux-persist/integration/react';
+import { curryGetDefaultMiddleware } from '@reduxjs/toolkit/dist/getDefaultMiddleware';
+
+const persistConfig= {key: "root", storage , version:1};
+const persistedReducer=persistReducer(persistConfig,authReducer);
+const storage=configureStore({
+  reducer:persistedReducer,
+  middleware:(getDefaultMiddleware)=>
+  getDefaultMiddleware({
+    serializableCheck:{
+      ignoreActions:[ FLUSH,
+        REHYDRATE,
+        PAUSE,
+        PERSIST,
+        PURGE,
+        REGISTER]
+    }
+  })
+})
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={storage}>
+      <PersistGate loading={null} persistor={persistStore(storage)}>
+      <App />
+      </PersistGate>
+    </Provider>
   </React.StrictMode>
 );
 
