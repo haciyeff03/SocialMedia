a7ea7d1ae49ee90c0367e656df9cc8e0f2d21f32
Your Name <haciyevqurban97@gmail,com>
2023-08-19 02:33:28 +0400

    navbar a smal part is ready

--- client/src/Layout/Navbar/Navbar.jsx
+++ client/src/Layout/Navbar/Navbar.jsx
@@ -1,10 +1,56 @@
 import React from 'react';
 import "./navbar.scss";
-import { useState , useEffect } from 'react';
-import { Box } from '@mui/material';
+import { useState, useEffect } from 'react';
+import {
+  Box,
+  IconButton,
+  InputBase,
+  Typography,
+  Select,
+  MenuItem,
+  FormControl,
+  useTheme,
+  useMediaQuery
+} from '@mui/material';
+import {
+  Search,
+  Message,
+  DarkMode,
+  Lightmode,
+  Notifications,
+  Help,
+  Menu,
+  Close
+} from "@mui/icons-material";
+import { UseSelector,useDispatch, useSelector } from 'react-redux';
+import { setMode, setLogout } from 'state';
+import { useNavigate } from 'react-router-dom';
+import FlexBetween from 'components/FlexBetween/FlexBetween';
 const Navbar = () => {
+  const [isMobileMenuToggle,setIsMobileMenuToggle]=useState(false);
+  const dispatch=useDispatch();
+  const navigate=useNavigate();
+  const user=useSelector((state)=>state.user);
+  const isNonMobileScreens=useMediaQuery("(min-width:1000px)");
+
+  const theme=useTheme();
+
+  const neutralLight=theme.palette.neutral.light;
+  const dark=theme.palette.neutral.dark;
+  const background=theme.palette.background.default;
+  const primaryLight=theme.palette.primary.light;
+  const alt=theme.palette.background.alt; 
+
+  const fullName= `${user.firstName} ${user.lastName}`
+
   return (
-    <div>Navbar</div>
+   <FlexBetween padding="1rem 6%" backgroundColor={alt}>
+    <FlexBetween gap="1.75rem">
+      <Typography fontSize="clamp(1rem,2rem,2.25rem)" fontWeight="bold">
+        SocialMedia
+      </Typography>
+    </FlexBetween>
+    </FlexBetween>
   )
 }
 
