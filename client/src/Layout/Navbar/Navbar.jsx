17f6155f86cc6b5f4951d60e10f1552d2b2e4cbc
Your Name <haciyevqurban97@gmail,com>
2023-08-20 02:11:31 +0400

    navbar darkmode state

--- client/src/Layout/Navbar/Navbar.jsx
+++ client/src/Layout/Navbar/Navbar.jsx
@@ -22,34 +22,62 @@ import {
   Menu,
   Close
 } from "@mui/icons-material";
-import { UseSelector,useDispatch, useSelector } from 'react-redux';
+import { UseSelector, useDispatch, useSelector } from 'react-redux';
 import { setMode, setLogout } from 'state';
 import { useNavigate } from 'react-router-dom';
 import FlexBetween from 'components/FlexBetween/FlexBetween';
 const Navbar = () => {
-  const [isMobileMenuToggle,setIsMobileMenuToggle]=useState(false);
-  const dispatch=useDispatch();
-  const navigate=useNavigate();
-  const user=useSelector((state)=>state.user);
-  const isNonMobileScreens=useMediaQuery("(min-width:1000px)");
+  const [isMobileMenuToggle, setIsMobileMenuToggle] = useState(false);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const user = useSelector((state) => state.user);
+  const isNonMobileScreens = useMediaQuery("(min-width:1000px)");
 
-  const theme=useTheme();
+  const theme = useTheme();
 
-  const neutralLight=theme.palette.neutral.light;
-  const dark=theme.palette.neutral.dark;
-  const background=theme.palette.background.default;
-  const primaryLight=theme.palette.primary.light;
-  const alt=theme.palette.background.alt; 
+  const neutralLight = theme.palette.neutral.light;
+  const dark = theme.palette.neutral.dark;
+  const background = theme.palette.background.default;
+  const primaryLight = theme.palette.primary.light;
+  const alt = theme.palette.background.alt;
 
-  const fullName= `${user.firstName} ${user.lastName}`
+  const fullName = `${user.firstName} ${user.lastName}`
 
   return (
-   <FlexBetween padding="1rem 6%" backgroundColor={alt}>
-    <FlexBetween gap="1.75rem">
-      <Typography fontSize="clamp(1rem,2rem,2.25rem)" fontWeight="bold">
-        SocialMedia
-      </Typography>
-    </FlexBetween>
+    <FlexBetween padding="1rem 6%" backgroundColor={alt}>
+      <FlexBetween gap="1.75rem">
+        <Typography fontSize="clamp(1rem,2rem,2.25rem)" fontWeight="bold" color="primary"
+          onClick={() => navigate("/home")}
+          sx={{
+            "&:hover": {
+              color: primaryLight,
+              cursor: "pointer"
+            },
+          }}>
+          SocialMedia
+        </Typography>
+        {isNonMobileScreens && (
+          <FlexBetween backgroundColor={neutralLight} borderRadius="9px" gap="3rem" padding="0.1rem 1.5rem">
+            <InputBase placeholder='Search...'/>
+            <IconButton>
+              <Search/>
+            </IconButton>
+          </FlexBetween>
+        )}
+      </FlexBetween>
+      {isNonMobileScreens ? (<FlexBetween gap="2rem">
+        <IconButton onClick={()=> dispatch(setMode())}>
+            {theme.palette.mode === "dark" ?
+            (<DarkMode sx={{fontSize:"25px"}}/>)
+          : (
+            <Lightmode sx={{color:dark , fontSize:"25px"}}/>
+          )}
+        </IconButton>
+        </FlexBetween>):(
+          <IconButton>
+
+          </IconButton>
+        )}
     </FlexBetween>
   )
 }
