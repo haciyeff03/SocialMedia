26a6c570d73c6896220494f7b98353589a18e9d7
Your Name <haciyevqurban97@gmail,com>
2023-09-03 21:46:16 +0400

    userwidget is completed

--- client/src/components/UserImage/UserImage.jsx
+++ client/src/components/UserImage/UserImage.jsx
@@ -4,7 +4,8 @@ import { Box } from "@mui/material";
 const UserImage=({image,size="60px"})=> {
 return(
     <Box width={size} height={size}>
-         <img src=" " alt=""
+         <img src={`http://localhost:3001/assets/${image}`} 
+         alt="user"
          style={{objectFit:"cover",
          borderRadius:"50%"}}
          width={size}
